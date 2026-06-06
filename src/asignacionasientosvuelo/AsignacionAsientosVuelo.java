a1be03b3453b7171a8fe2e918d555ff7302789d6
bcrispin317-cyber <bcrispin317@gmail.com>
2026-06-05 22:00:53 -0600

    Conversión de letras de asiento a columnas

--- src/asignacionasientosvuelo/AsignacionAsientosVuelo.java
+++ src/asignacionasientosvuelo/AsignacionAsientosVuelo.java
@@ -40,11 +40,40 @@ public class AsignacionAsientosVuelo {
             System.out.println();
         }
     }
+    public static int obtenerColumna(char letra) {
+
+    switch (Character.toUpperCase(letra)) {
+
+        case 'A':
+            return 0;
+
+        case 'B':
+            return 1;
+
+        case 'C':
+            return 2;
+
+        case 'D':
+            return 3;
+
+        case 'E':
+            return 4;
+
+        case 'F':
+            return 5;
+
+        default:
+            return -1;
+    }
+}
 
     public static void main(String[] args) {
 
         inicializarCabina(cabina);
         mostrarCabina(cabina);
+        System.out.println(obtenerColumna('A'));
+        System.out.println(obtenerColumna('C'));
+        System.out.println(obtenerColumna('F'));
 
     }
 }
